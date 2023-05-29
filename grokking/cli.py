77d8934b334074ad09783337bd76d14656fea16f
JohnNehls <nehls@email.arizona.edu>
2023-05-29 12:10:02 -0700

    Modify defaults to a) better match the paper's Fig1 b) use cuda
    I wanted the code to reproduce Fig1 out of the box. This commit is a step in that
    direction.
    
    Weight decay is set to default to 0-- turned off. I assume this as
    Fig2 shows how weight decal greatly increases generalization when used (>0).

--- grokking/cli.py
+++ grokking/cli.py
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     parser.add_argument("--num_heads", type=int, default=4)
     parser.add_argument("--batch_size", type=int, default=512)
     parser.add_argument("--learning_rate", type=float, default=1e-3)
-    parser.add_argument("--weight_decay", type=float, default=1)
-    parser.add_argument("--num_steps", type=int, default=1e5)
-    parser.add_argument("--device", type=str, default="cpu")
+    parser.add_argument("--weight_decay", type=float, default=0)
+    parser.add_argument("--num_steps", type=int, default=1e6)
+    parser.add_argument("--device", type=str, default="cuda")
     args = parser.parse_args()
 
     main(args)
